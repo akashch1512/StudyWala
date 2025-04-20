ce1f707f3d5e586a62684cbe38d889f794cb4b6a
akash <akashchaudhari1012@gmail.com>
2025-04-20 15:38:58 +0530

    signup removed

--- StudyWala/accounts/urls.py
+++ StudyWala/accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
+    # path('signup/', views.signup, name='signup'),
     path('password_reset/', views.custom_password_reset, name='password_reset'),
 ]
 
