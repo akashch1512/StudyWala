30dc1f5e8b789b7ddc71ab8287cffcb98396fea7
akash <akashchaudhari1012@gmail.com>
2025-04-20 15:33:56 +0530

    signup removed

--- StudyWala/accounts/views.py
+++ StudyWala/accounts/views.py
@@ -11,16 +11,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to login page after successful signup
-    else:
-        form = UserCreationForm()
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('login')  # Redirect to login page after successful signup
+#     else:
+#         form = UserCreationForm()
     
-    return render(request, 'registration/signup.html', {'form': form})
+#     return render(request, 'registration/signup.html', {'form': form})
 
 
 
