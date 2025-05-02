9060b92fc2ec7dbce7c02b619c713ad0cb7bbe7d
akash <akashchaudhari1012@gmail.com>
2025-05-03 00:19:44 +0530

    minor chnages

--- api/index.py
+++ api/index.py
@@ -1,1 +1,13 @@
-from StduyWala.wsgi import application
+import os
+import sys
+
+# Add root directory to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Set Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StduyWala.settings")
+
+# Import WSGI application
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()  # 👈 This is the key! Vercel looks for `app`
