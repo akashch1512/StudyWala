6f5dd2d9531ee5d1ba97bf3e975f6ece82a15915
akash <akashchaudhari1012@gmail.com>
2025-05-03 00:10:03 +0530

    databse added

--- StduyWala/settings.py
+++ StduyWala/settings.py
@@ -80,13 +80,24 @@ WSGI_APPLICATION = 'StduyWala.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#local database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+from decouple import config
+import dj_database_url
+# Production Database
+DATABASES = {
+    'default': dj_database_url.parse(
+        config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
